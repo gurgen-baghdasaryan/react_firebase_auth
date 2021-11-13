6b9dbfc99f88aa1b61b3c0b3a6f4a4c1eb57564d
gugohh <oohhgug@gmail.com>
2021-11-13 20:25:48 +0100

    routing done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+
+    <div className="row mt-5">
+      <div className="col"></div>
+      <div className="col">
+        <form className='form-group'>
+          <input className='form-control' type="text" placeholder="Introduce the Email"/>
+          <input className='form-control mt-3' type="text" placeholder="Introduce the Password" />
+          <input className='btn btn-dark  btn-responsive btninter center mt-3' type="submit" value="User Register"/>
+        </form>
+      </div>
+      <div className="col"></div>
+    </div>
+
+  );
 };
 
 export default Login;
