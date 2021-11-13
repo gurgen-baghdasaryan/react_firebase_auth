49dee69c051cd33a4483a36c72fde4e3b707d2a5
gugohh <oohhgug@gmail.com>
2021-11-13 19:24:03 +0100

    routing done

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div>
+
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/Admin">Admin</Link>
+          </li>
+        </ul>
+      </nav>
+
+    </div>);
 };
 
 export default Menu;
