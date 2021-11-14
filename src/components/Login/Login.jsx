067db0cfc93d9e3a13abf3b3a7e72e42806c7aab
gugohh <oohhgug@gmail.com>
2021-11-14 12:28:13 +0100

    const user = await createUserWithEmailAndPassword()

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../../firebaseconfig";
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+
+  const userRegister = async(e) => {
+    e.preventDefault()
+    try{
+    const user = await createUserWithEmailAndPassword(auth, email, pass)
+      console.log(user);
+    }catch(error){
+      console.log(error.message);
+    }
+
+
+
+
+  }
+
+
   return (
 
     <div className="row mt-5">
       <div className="col"></div>
       <div className="col">
-        <form className='form-group'>
-          <input className='form-control' type="text" placeholder="Introduce the Email"/>
-          <input className='form-control mt-3' type="text" placeholder="Introduce the Password" />
-          <input className='btn btn-dark  btn-responsive center mt-3' type="submit" value="User Register"/>
+        <form className='form-group' onSubmit={userRegister} >
+          <input
+            onChange={(e) => { setEmail(e.target.value) }}
+            className='form-control'
+            type="text"
+            placeholder="Introduce the Email"
+            value={email}
+          />
+          <input
+            onChange={(e) => { setPass(e.target.value) }}
+            className='form-control mt-3'
+            type="password"
+            placeholder="Introduce the Password"
+            
+            />
+          <input className='btn btn-dark  btn-responsive center mt-3'
+            type="submit"
+            value="User Register" />
         </form>
       </div>
       <div className="col"></div>
